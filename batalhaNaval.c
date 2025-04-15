93e98855cbc5b4c9bc5885a61fa1c4599b2f4646
gabrielvbn <gabriellucena_98@hotmail.com>
2025-04-15 19:14:34 +0000

    Desafio Nv Iniciante

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+//Variavel para imprimir para o usuario a localização das linhas e colunas
+    char linha [10] = {'A','B','C','D','E','F','G','H','I','J'};
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+//Criando tabuleiro (Matriz 10x10)
+    int tabuleiro [10][10] = {
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,3,0,0},
+        {0,0,0,0,0,0,0,3,0,0},
+        {0,0,0,0,0,0,0,3,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,3,3,3,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+        
+    };
+//Imprimindo na tela o resultado
+printf ("*****BATALHA NAVAL*****\n");
+printf ("O numero 0 e: AGUA e o 3: e o NAVIO!\n");
+printf ("   %c,%c,%c,%c,%c,%c,%c,%c,%c,%c\n", linha [0], linha [1], linha [2], linha [3], linha [4], linha [5], linha [6], linha [7], linha [8], linha [9]);
+printf ("1- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[0][0], tabuleiro [0][1], tabuleiro [0][2], tabuleiro [0][3], tabuleiro [0][4], tabuleiro [0][5], tabuleiro [0][6], tabuleiro [0][7], tabuleiro [0][8], tabuleiro [0][9]);
+printf ("2- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[1][0], tabuleiro [1][1], tabuleiro [1][2], tabuleiro [1][3], tabuleiro [1][4], tabuleiro [1][5], tabuleiro [1][6], tabuleiro [1][7], tabuleiro [1][8], tabuleiro [1][9]);
+printf ("3- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[2][0], tabuleiro [2][1], tabuleiro [2][2], tabuleiro [2][3], tabuleiro [2][4], tabuleiro [2][5], tabuleiro [2][6], tabuleiro [2][7], tabuleiro [2][8], tabuleiro [2][9]);    
+printf ("4- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[3][0], tabuleiro [3][1], tabuleiro [3][2], tabuleiro [3][3], tabuleiro [3][4], tabuleiro [3][5], tabuleiro [3][6], tabuleiro [3][7], tabuleiro [3][8], tabuleiro [3][9]); 
+printf ("5- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[4][0], tabuleiro [4][1], tabuleiro [4][2], tabuleiro [4][3], tabuleiro [4][4], tabuleiro [4][5], tabuleiro [4][6], tabuleiro [4][7], tabuleiro [4][8], tabuleiro [4][9]);   
+printf ("6- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[5][0], tabuleiro [5][1], tabuleiro [5][2], tabuleiro [5][3], tabuleiro [5][4], tabuleiro [5][5], tabuleiro [5][6], tabuleiro [5][7], tabuleiro [5][8], tabuleiro [5][9]); 
+printf ("7- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[6][0], tabuleiro [6][1], tabuleiro [6][2], tabuleiro [6][3], tabuleiro [6][4], tabuleiro [6][5], tabuleiro [6][6], tabuleiro [6][7], tabuleiro [6][8], tabuleiro [6][9]); 
+printf ("8- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[7][0], tabuleiro [7][1], tabuleiro [7][2], tabuleiro [7][3], tabuleiro [7][4], tabuleiro [7][5], tabuleiro [7][6], tabuleiro [7][7], tabuleiro [7][8], tabuleiro [7][9]); 
+printf ("9- %d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[8][0], tabuleiro [8][1], tabuleiro [8][2], tabuleiro [8][3], tabuleiro [8][4], tabuleiro [8][5], tabuleiro [8][6], tabuleiro [8][7], tabuleiro [8][8], tabuleiro [8][9]);
+printf ("10-%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" , tabuleiro[9][0], tabuleiro [9][1], tabuleiro [9][2], tabuleiro [9][3], tabuleiro [9][4], tabuleiro [9][5], tabuleiro [9][6], tabuleiro [9][7], tabuleiro [9][8], tabuleiro [9][9]);  
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
-    return 0;
+return 0;
 }
